8c3b7655f8740af6b39942d53f13d03620136ec6
MeerKatnip <amycodesme@gmail.com>
2020-05-14 14:45:22 -0500

    Update to app.js, added get requests

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ app.get('/',(req,res) => {
     res.render('index')
 })
 
-app.get('/trip',(req,res) => {
-    res.render('trip')
-    console.log('hello world')
+app.get('/all-trips',(req,res) => {
+    res.render('all-trips')
+    console.log('viewing all trips')
 })
 
+app.get('/add-trip',(req,res) => {
+    res.render('add-trip')
+    console.log('adding a trip')
+})
+
+app.get('/delete-trip',(req,res) => {
+    res.render('delete-trip')
+    console.log('deleting a trip')
+})
 
 
 
